418c2fb42ee67b70ebd1315e0e7593edbe65a73e
ANDREI <ugryumovandrej@yandex.ru>
2022-12-26 03:12:25 +0300

    Task1 done

---
+++ Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿ //Ввод n . вывод все натуральные от n до 1. n=5 5 4 3 2 1
+
+void NatChisla (int n)
+{
+    if(n ==1) Console.Write(n);
+    else
+    {
+        Console.Write($"{n} ");
+        NatChisla(n-1);
+    }
+} 
+  NatChisla(Convert.ToInt32(Console.ReadLine()));
