0f6e2064f3fef621e36b71a86011451ff7844b8b
ANDREI <ugryumovandrej@yandex.ru>
2022-12-26 03:30:34 +0300

    Task2 done

---
+++ Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Вводим m и n . Вывод сумма от m до n; 1 15 => 120; 4 8 => 30
+int Summ(int m, int n)
+{
+    if(n==m) return(n);
+    return n + Summ(m, n-1);
+}
+Console.WriteLine("Введите 1 число");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2 число");
+int n2 = Convert.ToInt32(Console.ReadLine());
+int summa = Summ(n1, n2);
+Console.Write(summa);
